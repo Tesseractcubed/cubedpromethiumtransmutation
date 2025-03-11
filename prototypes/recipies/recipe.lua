2f8231570abeb9c9e3b82a45bc9bd7f3fbd7d20b
Tesseractcubed <52340202+Tesseractcubed@users.noreply.github.com>
2025-03-10 20:03:42 -0500

    Updates
    Updates for v0.0.1

--- prototypes/recipies/recipe.lua
+++ prototypes/recipies/recipe.lua
@@ -5,12 +5,15 @@ data:extend({
 	{
         type = "recipe",
 		name = "uranium-transmutation",
-        icon = "__base__/graphics/icons/nuclear-fuel-reprocessing.png",
+        icon = "__base__/graphics/icons/nuclear-fuel-reprocessing.png", --Temporary Filke
         category = "centrifuging",
-		enabled = false,
+		enabled = false, 
 		energy_required = 30,
 		ingredients = {
-			{ type = "item", name = "speed-module-3", amount = 1 },
+			{ type = "item", name = "depleted-uranium-fuel-cell", amount = 1 },
+            { type = "item", name = "raw-fish", amount = 5 },
+            { type = "item", name = "promethium-asteroid-chunk", amount = 1 },
+            --Temporary recipe
 		},
         main_product = "",
 		results = {
