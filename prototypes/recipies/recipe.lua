7f00d1484dd224a914941a3b0652d2c2ca4edda7
Tesseractcubed <52340202+Tesseractcubed@users.noreply.github.com>
2025-03-10 20:38:39 -0500

    Added transmutation for other planets
    Tungsten, Scrap, and Lithium transmutation

--- prototypes/recipies/recipe.lua
+++ prototypes/recipies/recipe.lua
@@ -7,17 +7,75 @@ data:extend({
 		name = "uranium-transmutation",
         icon = "__base__/graphics/icons/nuclear-fuel-reprocessing.png", --Temporary Filke
         category = "centrifuging",
-		enabled = false, 
-		energy_required = 30,
+		energy_required = 60,
 		ingredients = {
 			{ type = "item", name = "depleted-uranium-fuel-cell", amount = 1 },
             { type = "item", name = "raw-fish", amount = 5 },
             { type = "item", name = "promethium-asteroid-chunk", amount = 1 },
-            --Temporary recipe
 		},
         main_product = "",
 		results = {
 			{ type = "item", name = "uranium-ore", amount = 10 },
 		},
+		enabled = false,
+		allow_productivity = true,
+		auto_recycle = false,
+    },
+	{
+        type = "recipe",
+		name = "tungsten-transmutation",
+        icon = "__base__/graphics/icons/nuclear-fuel-reprocessing.png", --Temporary Filke
+        category = "metallurgy",
+		energy_required = 60,
+		ingredients = {
+			{ type = "item", name = "tungsten-plate", amount = 2},
+			{ type = "fluid", name = "lava", amount = 1000},
+			{ type = "item", name = "coal", amount = 5},
+            { type = "item", name = "promethium-asteroid-chunk", amount = 1 },
+		},
+        main_product = "",
+		results = {
+			{ type = "item", name = "tungsten-ore", amount = 10 },
+		},
+		enabled = false,
+		allow_productivity = true,
+		auto_recycle = false,
+    },
+	{
+        type = "recipe",
+		name = "scrap-transmutation",
+        icon = "__base__/graphics/icons/nuclear-fuel-reprocessing.png", --Temporary Filke
+        category = "electromagnetics",
+		energy_required = 60,
+		ingredients = {
+			{ type = "fluid", name = "sulfuric-acid", amount = 25},
+			{ type = "fluid", name = "electrolyte", amount = 20},
+            { type = "item", name = "promethium-asteroid-chunk", amount = 1 },
+		},
+        main_product = "",
+		results = {
+			{ type = "item", name = "scrap", amount = 10 },
+		},
+		enabled = false,
+		allow_productivity = true,
+		auto_recycle = false,
+    },
+	{
+        type = "recipe",
+		name = "lithium-transmutation",
+        icon = "__base__/graphics/icons/nuclear-fuel-reprocessing.png", --Temporary Filke
+        category = "cryogenics",
+		energy_required = 60,
+		ingredients = {
+
+            { type = "item", name = "promethium-asteroid-chunk", amount = 1 },
+		},
+        main_product = "",
+		results = {
+			{ type = "fluid", name = "lithium-brine", amount = 50 },
+		},
+		enabled = false,
+		allow_productivity = true,
+		auto_recycle = false,
     }
 })
